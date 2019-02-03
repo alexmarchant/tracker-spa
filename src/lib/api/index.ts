dc2a8aa47637645b556c0e06d1c8a380dc7a2a35
Alex Marchant <alexjmarchant@gmail.com>
2019-02-03 02:04:58 -0500

    Update localhost route

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,7 +1,7 @@
 import { index, create } from './calories'
 
 export const BASE_URL = document.location.host.match('localhost')
-  ? document.location.host
+  ? `http://localhost:3000`
   : 'http://api.tracker.alexmarchant.com'
 
 export default {
